856d644106b61be1396ee8ab92f12fe891a4935b
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-06-24 23:18:35 -0500

    display score

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var myQuestion=0;
-var veiwAnswers=0;
-var rightAnswers=0;
-var quizDone=false;
-var myAnwer=[];
-var c=180;
+var myQuestion = 0;
+var veiwAnswers = 0;
+var rightAnswers = 0;
+var quizDone = false;
+var myAnwer = [];
+var c = 180;
 var t;
-
 
 // var currentQuestion = 0;
 // var grade = 0;
@@ -37,99 +36,113 @@ var questions = [
     correctAnswers: 1,
   },
 ];
-$(document).ready(function()
-    {
-        displayMyQuestion();
-        $(this).find(".quizmessage").hide();
-        $(this).find(".goBack").attr('disabled', 'disabled');
-        timecount();
-        $(this).find(".goBack").on("click",function() {
-		
-        	if(quizDone===false)
-     	{
-            if(myQuestion==0) {return false;}
-            if(myQuestion==1) {$("goBack").attr('hide', 'hide');
+$(document).ready(function () {
+  displayMyQuestion();
+  $(this).find(".quizmessage").hide();
+  $(this).find(".goBack").attr("disabled", "disabled");
+  timecount();
+  $(this).find(".myanswer").on("click",
+    function () {
+      if (quizDone === false) {
+        if (myQuestion == 0) {
+          return false;
+        }
+        if (myQuestion == 1) {
+          $("goBack").attr("hide", "hide");
         }
         myQuestion--;
-        if (myQuestion<questions.length)
-        {displayMyQuestion();
-	   }     
-		}
-		else {
-		if(veiwAnswers===3) {return false;}
-		myQuestion =0;veiwAnswers =3
-		viewHighScores();
-		}  
-	});
-	
-	
+        if (myQuestion < questions.length) {
+          displayMyQuestion();
+        }
+      } else {
+        if (veiwAnswers === 3) {
+          return false;
+        }
+        myQuestion = 0;
+        veiwAnswers = 3;
+        viewHighScores();
+      }
+ } );
+  $(this).find(".myanswer").on("click",
+    function () {
+      if (quizDone === false) {
+        var val = $("input[type='radio']:checked").val();
+        if (val == undefined) {
+          $(document).find(".message").text("please select an answer");
+		$(document).find("message").show();
+        }
+	//    break
+	   else if(myQuestion<question.length){
+	displayMyQuestion();
+      }
+	 else
+	{displayscore();
+		$('#timeclock').html('quiz time complete!');
+		$("#timer").html("you scored: " rightAnswers + "out of:" + questions.length)
+    };
+	 }
+  // function startTest() {
+  //   currentQuestion = 0;
+  //   questionNum.innerHTML = questions[currentQuestion].question;
+  //   // change button  name goBack.
+  //   trueBtn.innHTML = questions[currentQuestion].answers[0].option;
+  //   trueBtn.onclick = () => {
+  //     var ano = 0;
+  //     if (questions[currentQuestion].answers[ano].answer) {
+  //       if (grade < 3) {
+  //         score++;
+  //       }
+  //     }
+  //     UserScores.innerHTML = score;
+  //     if (currentQuestion < 2) {
+  //       next();
+  //     }
+  //   };
+  // }
+  // falseBtn.HTML = questions[currentQuestion].answers[1].option;
+  // falseBtn.onclick = () => {
+  //   var ano = 1;
+  //   if (questions[currentQuestion].answers[ano].answer) {
+  //     if (grade < 3) {
+  //       score++;
+  //     }
+  //   }
+  //   goBack.classList.add("hide");
+  // };
+  // startQuiz();
 
+  // function restart() {
+  //   currentQuestion = 0;
+  //   goBack.classList.remove("hide");
+  //   trueBtn.classList.remove("hide");
+  //   falseBtn.classList.remove("hide");
+  //   clearHighSCores.classList.remove("hide");
+  //   submitScores.classList.remove("hide");
+  //   startQuiz.classList.remove("hide");
+  //   score = 0;
+  //   UserScores.innerHTML = score;
+  //   startQuiz();
+  // }
 
-// function startTest() {
-//   currentQuestion = 0;
-//   questionNum.innerHTML = questions[currentQuestion].question;
-//   // change button  name goBack.
-//   trueBtn.innHTML = questions[currentQuestion].answers[0].option;
-//   trueBtn.onclick = () => {
-//     var ano = 0;
-//     if (questions[currentQuestion].answers[ano].answer) {
-//       if (grade < 3) {
-//         score++;
-//       }
-//     }
-//     UserScores.innerHTML = score;
-//     if (currentQuestion < 2) {
-//       next();
-//     }
-//   };
-// }
-// falseBtn.HTML = questions[currentQuestion].answers[1].option;
-// falseBtn.onclick = () => {
-//   var ano = 1;
-//   if (questions[currentQuestion].answers[ano].answer) {
-//     if (grade < 3) {
-//       score++;
-//     }
-//   }
-//   goBack.classList.add("hide");
-// };
-// startQuiz();
+  // establish button functions
+  const restartButton = document.getElementById("start-over");
+  const clearHighSCores = document.getElementById("clear-highscores");
+  const viewHighScores = document.getElementById("veiw-highscores");
+  const startQuiz = document.getElementById("start");
+  const goBack = document.getElementById("back");
+  const submitScores = document.getElementById("submit");
+  const UserScores = document.getElementById("user-score");
+  const questionNum = document.getElementById("question-text");
+  const trueBtn = document.getElementById("true");
+  const falseBtn = document.getElementById("false");
 
-// function restart() {
-//   currentQuestion = 0;
-//   goBack.classList.remove("hide");
-//   trueBtn.classList.remove("hide");
-//   falseBtn.classList.remove("hide");
-//   clearHighSCores.classList.remove("hide");
-//   submitScores.classList.remove("hide");
-//   startQuiz.classList.remove("hide");
-//   score = 0;
-//   UserScores.innerHTML = score;
-//   startQuiz();
-// }
+  // establish actions of the click of buttons
 
-
-
-
-
-// establish button functions
-const restartButton = document.getElementById("start-over");
-const clearHighSCores = document.getElementById("clear-highscores");
-const viewHighScores = document.getElementById("veiw-highscores");
-const startQuiz = document.getElementById("start");
-const goBack = document.getElementById("back");
-const submitScores = document.getElementById("submit");
-const UserScores = document.getElementById("user-score");
-const questionNum = document.getElementById("question-text");
-const trueBtn = document.getElementById("true");
-const falseBtn = document.getElementById("false");
-
-// establish actions of the click of buttons
-
-restartButton.addEventListener("click", restart);
-clearHighSCores.addEventListener("click", clear - highscores);
-viewHighScores.addEventListener("click", view - highscores);
-submitScores.addEventListener("click", submit);
-startQuiz.addEventListener("click", start);
-goBack.addEventListener("click", back);
-submitScores.addEventListener("click", submit);
+  restartButton.addEventListener("click", restart);
+  clearHighSCores.addEventListener("click", clear - highscores);
+  viewHighScores.addEventListener("click", view - highscores);
+  submitScores.addEventListener("click", submit);
+  startQuiz.addEventListener("click", start);
+  goBack.addEventListener("click", back);
+  submitScores.addEventListener("click", submit);
+});
