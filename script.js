29359c0a3a2555f3866599cec745f50e49e81162
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-06-27 10:15:21 -0500

    dispaly question and initial puniment started

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ var c = 180;
 var t;
 var queCount = 0;
 var queNum = 1;
-var UserScores = 0;
+var userScoress = 0;
 var countDown;
 var counterLine;
 var widthValue = 0;
 var currentQuestionIndex = 0;
 var score= 0;
+var questionText = document.getElementById("question_text")
+var choiceList= document.querySelector(".choiceList")
 
 var questions = [
   {
     question: "1.is coding fun ?",
     answers: ["yes", "no", "maybe", "whateva"],
-    correctAnswers: 1,
+    correctAnswers: "yes",
   },
   {
     question: "2.whast your favorite movie ?",
@@ -42,6 +44,35 @@ var questions = [
   },
 ];
 
+questionText.textContent=questions[currentQuestionIndex].question
+var answerChoice1 = document.createElement("button");
+var answerChoice2 = document.createElement("button");
+var answerChoice3 = document.createElement("button");
+var answerChoice4 = document.createElement("button");
+
+answerChoice1.textContent=questions[currentQuestionIndex].answers[0];
+answerChoice2.textContent=questions[currentQuestionIndex].answers[1];
+answerChoice3.textContent=questions[currentQuestionIndex].answers[2];
+answerChoice4.textContent=questions[currentQuestionIndex].answers[3];
+
+choiceList.appendChild(answerChoice1);
+choiceList.appendChild(answerChoice2);
+choiceList.appendChild(answerChoice3);
+choiceList.appendChild(answerChoice4);
+
+choiceList.addEventListener("click", function(event){
+
+var element =event.target;
+
+if (element.matches("button")){
+  console.log(event.target.textContent);
+  currentQuestionIndex++
+  console.log(currentQuestionIndex);
+}
+
+})
+
+
 
 
 
@@ -53,16 +84,16 @@ var myfunc = setInterval(function () {
   var timeValue = countDownDate - now;
 
   // Calculating the days, hours, minutes and seconds left
-   var days = Math.floor(timeValue / (10000 * 6000 * 6000 * 24));
-   var hours = Math.floor((timeValue % (100 * 60 * 60 * 24)) / (10000 * 60 * 60));
-   var minutes = Math.floor((timeValue % (10 * 60 * 60)) / (10000 * 60));
-   var seconds = Math.floor((timeValue % (1080 * 60)) / 1000);
-  // var days = Math.floor(timeValue / (10000 * 6000 * 6000 * 24));
-  // var hours = Math.floor(
-  //   (timeValue % (100 * 60 * 60 * 24)) / (10000 * 60 * 60)
-  // );
-  // var minutes = Math.floor((timeValue % (10 * 60 * 60)) / (10000 * 60));
-  // var seconds = Math.floor((timeValue % (110 * 60)) / 1000);
+  //  var days = Math.floor(timeValue / (10000 * 6000 * 6000 * 24));
+  //  var hours = Math.floor((timeValue % (100 * 60 * 60 * 24)) / (10000 * 60 * 60));
+  //  var minutes = Math.floor((timeValue % (10 * 60 * 60)) / (10000 * 60));
+  //  var seconds = Math.floor((timeValue % (1080 * 60)) / 1000);
+  var days = Math.floor(timeValue / (10000 * 6000 * 6000 * 24));
+  var hours = Math.floor(
+    (timeValue % (100 * 60 * 60 * 24)) / (10000 * 60 * 60)
+  );
+  var minutes = Math.floor((timeValue % (10 * 60 * 60)) / (10000 * 60));
+  var seconds = Math.floor((timeValue % (110 * 60)) / 1000);
   // Result is output to the specific element
   document.getElementById("days").innerHTML = days + "d ";
   document.getElementById("hours").innerHTML = hours + "h ";
@@ -70,7 +101,7 @@ var myfunc = setInterval(function () {
   document.getElementById("secs").innerHTML = seconds + "s ";
 
   // Display the message when countdown is over
-  if (timeValue < 0) {
+  if (timeValue === 0) {
     clearInterval(myfunc);
     document.getElementById("days").innerHTML = "";
     document.getElementById("hours").innerHTML = "";
@@ -78,7 +109,20 @@ var myfunc = setInterval(function () {
     document.getElementById("secs").innerHTML = "";
     document.getElementById("end").innerHTML = "TIME UP!!";
   }
+
+  // punimnet for incorrect answers
+  // if (count > 0) {
+  //   count--;
+  //   counter.textContent = count;
+  //   localStorage.setItem("count", count);
+  // }
 }, 1000);
+
+  if (count > 0) {
+    count--;
+    counter.textContent = count;
+    localStorage.setItem("count", count);
+  }
 
 //  console.log( countDownDate);
 // const start_btn = document.querySelector(".start");
@@ -96,7 +140,7 @@ var myfunc = setInterval(function () {
 //   const startQuiz = document.getElementById("start");
 //   const goBack = document.getElementById("back");
 //   const submitScores = document.getElementById("submit");
-// //   const UserScores = document.getElementById("user-score");
+// //   const userScoress = document.getElementById("user-score");
 //   const questionNum = document.getElementById("question_text");
 //   const trueBtn = document.getElementById("true");
 //   const falseBtn = document.getElementById("false");
