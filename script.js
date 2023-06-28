5a7483f542c7bac7827341add7b766352939411e
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-06-28 18:16:26 -0500

    quiz functions now working on timer punishment

--- script.js
+++ script.js
@@ -17,71 +17,34 @@ var questions = [
       "arrayList.length=0",
       "arrayList.len(0)",
     ],
-    correctAnswer: "arrayList(0)",
+    correctAnswer: "arrayList[]",
   },
   {
     question:
       "3. What function to add an element at the begining of an array and one at the end?",
     choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
-    correctAnswer: 1,
+    correctAnswer: "push,unshift",
   },
   {
     question: "4. What will this output? let a = [1, 2, 3]; console.log(a[6]);",
     choices: ["undefined", "0", "prints nothing", "Syntax error"],
-    correctAnswer: 0,
+    correctAnswer: "undefined",
   },
   {
     question:
       "5. What would following code return? console.log(typeof typeof 1);",
     choices: ["string", "number", "Syntax error", "undefined"],
-    correctAnswer: 0,
-  },
-  {
-    question: "6. Which software company developed JavaScript?",
-    choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
-    correctAnswer: 1,
-  },
-  {
-    question: "7. What would be the result of 3+2+'7'?",
-    choices: ["327", "12", "14", "57"],
-    correctAnswer: 3,
-  },
-  {
-    question:
-      "8. Look at the following selector: $('div'). What does it select?",
-    choices: [
-      "The first div element",
-      "The last div element",
-      "All div elements",
-      "Current div element",
-    ],
-    correctAnswer: 2,
-  },
-  {
-    question: "9. How can a value be appended to an array?",
-    options: [
-      "arr(length).value;",
-      "arr[arr.length]=value;",
-      "arr[]=add(value);",
-      "None of these",
-    ],
-    correctAnswer: 1,
-  },
-  {
-    question:
-      "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
-    choices: ["'32'", "'122'", "'13'", "'14'"],
-    correctAnswer: 0,
+    correctAnswer: "string",
   },
 ];
-
+var hidden = false;
 var currentQuestion = 0;
 var viewingAns = 0;
-var correctAnswers = 0;
+var correctAnswers;
 var quizOver = false;
 var iSelectedAnswer = [];
-var c = 180;
-var t;
+var c = quizTime/2;
+var incorrect = [currentQuestion].choices;
 var queCount = 0;
 var queNum = 1;
 var UserScores = 0;
@@ -89,11 +52,11 @@ var countDown;
 var counterLine;
 var widthValue = 0;
 var score = 0;
-var quizTime = 6;
+var quizTime = 60;
 //selecting all required elements
 const start_btn = document.querySelector(".start");
 var questionText = document.getElementById(`question`);
-var choiceList = document.querySelector('.choiceList')
+var choiceList = document.querySelector(".choiceList");
 // const info_box = document.querySelector(".info_box");
 // const exit_btn = info_box.querySelector(".buttons .quit");
 // const nextQuestion = document.getElementById('nextQuestion');
@@ -106,7 +69,8 @@ const timeCount = document.querySelector(".timer");
 
 function showQuestions(currentQuestionIndex) {
   questionText.textContent = questions[currentQuestionIndex].question;
-  choiceList.innerHTML=""
+
+  choiceList.innerHTML = "";
   var answerChoice1 = document.createElement("button");
   var answerChoice2 = document.createElement("button");
   var answerChoice3 = document.createElement("button");
@@ -116,17 +80,23 @@ function showQuestions(currentQuestionIndex) {
   answerChoice2.textContent = questions[currentQuestionIndex].choices[1];
   answerChoice3.textContent = questions[currentQuestionIndex].choices[2];
   answerChoice4.textContent = questions[currentQuestionIndex].choices[3];
-
+  // correctanswerChoice1.textContent = questions[currentQuestionIndex[2]]
+  // correctanswerChoice2.textContent = questions[currentQuestionIndex].correctAnswer;
+  // correctanswerChoice3.textContent = questions[currentQuestionIndex].correctAnswer;
+  // correctanswerChoice4.textContent = questions[currentQuestionIndex].correctAnswer;
+  // correctanswerChoice4.textContent = questions[currentQuestionIndex].correctAnswer;
   choiceList.appendChild(answerChoice1);
   choiceList.appendChild(answerChoice2);
   choiceList.appendChild(answerChoice3);
   choiceList.appendChild(answerChoice4);
+  correctAnswers= questions[currentQuestionIndex].correctAnswer;
+  // console.log(correctAnswers);
 }
 
 function start() {
   showQuestions(currentQuestion);
   setTimer();
-  
+  start_btn.style.visibility = "hidden";
 }
 start_btn.addEventListener("click", start);
 // The data/time we want to countdown to
@@ -134,28 +104,44 @@ start_btn.addEventListener("click", start);
 // Run myfunc every second
 function setTimer() {
   var myTimer = setInterval(function () {
-      quizTime--;
-    timeCount.textContent= quizTime
-    if (quizTime <= 0){
-    clearInterval(myTimer);
+    quizTime--;
+    timeCount.textContent = quizTime;
+    if (quizTime <= 0) {
+      clearInterval(myTimer);
     }
-    console.log(quizTime);
+    
+    // console.log(quizTime);
   }, 1000);
 }
 
-choiceList.addEventListener("click", function (event) {
-  event.target
-  console.log(event.target);
-  if (event.target.matches("button") ){
-    // if i pick the right answer
-    currentQuestion++ ;
-    showQuestions(currentQuestion);
+choiceList.addEventListener(
+  "click",
+  function (event) {
+    event.target;
+    if (event.target.matches("button")) {
       // if i pick the right answer
-      // else we subtract time
-      currentQuestion++ ;
-    showQuestions(currentQuestion);
-  }
-})
+      if (event.target && correctAnswers) {
+        c;
+        currentQuestion++;
+      showQuestions(currentQuestion);
+      }
+      else if (event.target === correctAnswers) {
+        c;
+        currentQuestion++;
+      showQuestions(currentQuestion);
+      }
+      if (quizTime < 0) {
+        clearInterval(myTimer);
+        alert("time is up");
+      }
+      if (event.target && correctAnswers===false)
+      quizTime -= c;
+      console.log(c);
+      console.log(incorrect);
+      console.log(correctAnswers);
+      console.log(event.target);
+    }
+  },1000);
 
 
 
@@ -164,12 +150,8 @@ function nextQuestion() {
   document.addEventListener("click", choiceList);
   for (var i = 0; i < choices.length; i++) {
     const choiceList = choices[i++];
-    
   }
 }
-
-
-
 
 // function endOfQuiz(sendMessage) {
 //   if (quizTime==0){
